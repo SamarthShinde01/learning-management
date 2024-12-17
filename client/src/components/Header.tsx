6f56707bad217e1c354186709c660987f5bb3c32
Samarth Shinde <samarthsshinde@gmail.com>
2024-12-17 12:48:31 +0530

    [client] create a profile account

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Header({ title, subtitle, rightElement }: HeaderProps) {
+	return (
+		<div className="mb-7 flex justify-between items-center">
+			<div>
+				<h1 className="text-3xl font-bold text-white-50">{title}</h1>
+				<p className="text-sm text-gray-500 mt-1">{subtitle}</p>
+			</div>
+
+			{rightElement && <div>{rightElement}</div>}
+		</div>
+	);
+}
