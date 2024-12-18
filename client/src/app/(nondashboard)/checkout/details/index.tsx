d7059cf2905a4fe1d3ca0213b0e3f3710bf1168e
Samarth Shinde <samarthsshinde@gmail.com>
2024-12-18 15:33:04 +0530

    [client] useCheckoutNavigation hook

---
+++ client/src/app/(nondashboard)/checkout/details/index.tsx
@@ -0,0 +1,5 @@
+const CheckoutDetailsPage = () => {
+	return <></>;
+};
+
+export default CheckoutDetailsPage;
--- client/src/app/(nondashboard)/checkout/page.tsx
+++ client/src/app/(nondashboard)/checkout/page.tsx
@@ -1,9 +1,11 @@
 import Loading from "@/components/Loading";
 import WizardStepper from "@/components/WizardStepper";
+import { useCheckoutNavigation } from "@/hooks/useCheckoutNavigation";
 import { useUser } from "@clerk/nextjs";
 
 const CheckoutWizard = () => {
 	const { isLoaded } = useUser();
+	const { checkoutStep } = useCheckoutNavigation();
 
 	if (!isLoaded) return <Loading />;
 
