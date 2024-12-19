0507f1102ef625bdc7991f09b6254bd44d55218e
Samarth Shinde <samarthsshinde@gmail.com>
2024-12-19 19:47:13 +0530

    [server] course CRUD apis

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -57,6 +57,9 @@ export const api = createApi({
 	reducerPath: "api",
 	tagTypes: ["Courses", "Users"],
 	endpoints: (build) => ({
+		/* ==============
+		USERS
+		============== */
 		updateUser: build.mutation<User, Partial<User> & { userId: string }>({
 			query: ({ userId, ...updatedUser }) => ({
 				url: `/users/clerk/${userId}`,
@@ -65,6 +68,10 @@ export const api = createApi({
 			}),
 			invalidatesTags: ["Users"],
 		}),
+
+		/* ==============
+		COURSES
+		============== */
 		getCourses: build.query<Course[], { category?: string }>({
 			query: ({ category }) => ({
 				url: "courses",
@@ -77,6 +84,43 @@ export const api = createApi({
 			providesTags: (result, error, id) => [{ type: "Courses", id }],
 		}),
 
+		createCourse: build.mutation<
+			Course,
+			{ teacherId: string; teacherName: string }
+		>({
+			query: (body) => ({
+				url: "courses",
+				method: "POST",
+				body,
+			}),
+			invalidatesTags: ["Courses"],
+		}),
+
+		updateCourse: build.mutation<
+			Course,
+			{ courseId: string; formData: FormData }
+		>({
+			query: ({ courseId, formData }) => ({
+				url: `courses/${courseId}`,
+				method: "PUT",
+				body: formData,
+			}),
+			invalidatesTags: (result, error, { courseId }) => [
+				{ type: "Courses", id: courseId },
+			],
+		}),
+
+		deleteCourse: build.mutation<{ message: string }, string>({
+			query: (courseId) => ({
+				url: `courses/${courseId}`,
+				method: "DELETE",
+			}),
+			invalidatesTags: ["Courses"],
+		}),
+
+		/* ==============
+		TRANSACTIONS
+		============== */
 		getTransactions: build.query<Transaction[], string>({
 			query: (userId) => `transaction?userId=${userId}`,
 		}),
@@ -104,6 +148,9 @@ export const api = createApi({
 
 export const {
 	useUpdateUserMutation,
+	useCreateCourseMutation,
+	useUpdateCourseMutation,
+	useDeleteCourseMutation,
 	useGetCoursesQuery,
 	useGetCourseQuery,
 	useGetTransactionsQuery,
--- server/src/controllers/courseController.ts
+++ server/src/controllers/courseController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import Course from "../models/courseModel";
+import { v4 as uuidv4 } from "uuid";
+import { getAuth } from "@clerk/express";
 
 export const listCourses = async (req: Request, res: Response) => {
 	const { category } = req.query;
@@ -28,5 +30,117 @@ export const getCourse = async (req: Request, res: Response) => {
 		res.json({ message: "Course retrieved successfully", data: course });
 	} catch (error) {
 		res.status(500).json({ message: "Error retrieving course", error });
+	}
+};
+
+export const createCourse = async (req: Request, res: Response) => {
+	const { teacherId, teacherName } = req.body;
+
+	try {
+		if (!teacherId || !teacherName) {
+			res.status(400).json({ message: "Teacher Id and name is required" });
+			return;
+		}
+
+		const newCourse = new Course({
+			courseId: uuidv4(),
+			teacherId,
+			teacherName,
+			title: "Untitled Course",
+			description: "",
+			category: "Uncategorized",
+			image: "",
+			price: 0,
+			level: "Beginner",
+			status: "Draft",
+			sections: [],
+			enrollments: [],
+		});
+		await newCourse.save();
+
+		res.json({ message: "Course created successfully", data: newCourse });
+	} catch (error) {
+		res.json({ message: "Error creating course", error });
+	}
+};
+
+export const updateCourse = async (req: Request, res: Response) => {
+	const { courseId } = req.params;
+	const updateData = { ...req.body };
+	const { userId } = getAuth(req);
+
+	try {
+		const course = await Course.get(courseId);
+
+		if (!course) {
+			res.status(404).json({ message: "Course not found" });
+			return;
+		}
+
+		if (course.teacherId !== userId) {
+			res.status(403).json({ message: "Not authorized to update this course" });
+			return;
+		}
+
+		if (updateData.price) {
+			const price = parseInt(updateData.price);
+			if (isNaN(price)) {
+				res.status(400).json({
+					message: "Invalid price format",
+					error: "Price must be a valid number",
+				});
+
+				return;
+			}
+			updateData.price = price * 100;
+		}
+
+		if (updateData.sections) {
+			const sectionsData =
+				typeof updateData.sections === "string"
+					? JSON.parse(updateData.sections)
+					: updateData.sections;
+
+			updateData.sections = sectionsData.map((section: any) => ({
+				...section,
+				sectionId: section.sectionId || uuidv4(),
+				chapters: section.chapters.map((chapter: any) => ({
+					...chapter,
+					chapterId: chapter.chapterId || uuidv4(),
+				})),
+			}));
+		}
+
+		Object.assign(course, updateData);
+		await course.save();
+
+		res.json({ message: "Course updated successfully", data: course });
+	} catch (error) {
+		res.status(403).json({ message: "Error updating course", error });
+	}
+};
+
+export const deleteCourse = async (req: Request, res: Response) => {
+	const { courseId } = req.params;
+	const { userId } = getAuth(req);
+
+	try {
+		const course = await Course.get(courseId);
+
+		if (!course) {
+			res.status(404).json({ message: "Course not found" });
+			return;
+		}
+
+		if (course.teacherId !== userId) {
+			res.status(403).json({ message: "Not authorized to delete this course" });
+			return;
+		}
+
+		await Course.delete(courseId);
+
+		res.json({ message: "Course deleted successfully" });
+	} catch (error) {
+		res.status(403).json({ message: "Error deleting course", error });
 	}
 };
